74e4692a7c7d722c61afdabbb5c25acc683cbbaf
Dethblo <dethblo@live.com>
2022-01-13 17:42:49 -0500

    Illustrates writing JSON to a file from a dictionary object.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 import json
 
 
-# reads a json file and prints the contents
+# illustrates reading json string (both file and string)
 def read_json():
     # JSON string
     a = '{"name": "Bob", "languages": "English"}'
@@ -32,7 +32,30 @@ def read_json():
     # Closing file
     f.close()
 
+def write_json():
+    # Data to be written
+    dictionary = {
+        "name": "sathiyajith",
+        "rollno": 56,
+        "cgpa": 8.6,
+        "phonenumber": "9976770500"
+    }
+
+    # Explicitly Serializing json into an string object before writing
+    json_object = json.dumps(dictionary, indent=4)
+
+    # Writing to sample.json
+    print("\nWriting sample1.json file")
+    with open("sample1.json", "w") as outfile:
+        outfile.write(json_object)
+
+    # Letting json.dump directly write the dictionary to the file (without the conversion to an object)
+    print("\nWriting sample2.json file")
+    with open("sample2.json", "w") as outfile:
+        json.dump(dictionary, outfile)
+
 
 # main entry point
 if __name__ == '__main__':
     read_json()
+    write_json()
